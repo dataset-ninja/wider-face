eed7ca241451853ed74a3cf974e30ee704756fa9
Ars Vladimirov <grokhael@gmail.com>
2023-08-31 02:20:38 +0600

    PREVIEW_IMAGE_ID

--- src/convert.py
+++ src/convert.py
@@ -1,72 +1,103 @@
-import supervisely as sly
+import glob
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+# http://shuoyang1213.me/WIDERFACE/
+import shutil
+from collections import defaultdict
+from urllib.parse import unquote, urlparse
+
+import numpy as np
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from dotenv import load_dotenv
+from supervisely.io.fs import (
+    dir_exists,
+    file_exists,
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+)
 from tqdm import tqdm
 
-def download_dataset(teamfiles_dir: str) -> str:
-    """Use it for large datasets to convert them on the instance"""
-    api = sly.Api.from_env()
-    team_id = sly.env.team_id()
-    storage_dir = sly.app.get_data_dir()
+import src.settings as s
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
-        parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
-        file_name_with_ext = os.path.basename(parsed_url.path)
-        file_name_with_ext = unquote(file_name_with_ext)
-
-        sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-        local_path = os.path.join(storage_dir, file_name_with_ext)
-        teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-
-        fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-        with tqdm(desc=f"Downloading '{file_name_with_ext}' to buffer...", total=fsize) as pbar:
-            api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-        dataset_path = unpack_if_archive(local_path)
-
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
-        for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
-            local_path = os.path.join(storage_dir, file_name_with_ext)
-            teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-
-            if not os.path.exists(get_file_name(local_path)):
-                fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-                with tqdm(
-                    desc=f"Downloading '{file_name_with_ext}' to buffer...",
-                    total=fsize,
-                    unit="B",
-                    unit_scale=True,
-                ) as pbar:
-                    api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-
-                sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-                unpack_if_archive(local_path)
-            else:
-                sly.logger.info(
-                    f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
-                )
-
-        dataset_path = storage_dir
-    return dataset_path
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    # project_name = "FACE"
+    dataset_path = "APP_DATA/WIDER FACE/"
+    batch_size = 30
+    anns_folder = "APP_DATA/WIDER FACE/wider_face_split"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        bboxes = im_path_to_bboxes[image_path]
+        for bbox in bboxes:
+            left = int(bbox[0])
+            right = left + int(bbox[2])
+            top = int(bbox[1])
+            bottom = top + int(bbox[3])
+            rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+            label = sly.Label(rectangle, obj_class)
+            labels.append(label)
 
-    # return project
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
 
+    obj_class = sly.ObjClass("face", sly.Rectangle)
 
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
+
+    ds_to_anns = {
+        "train": "wider_face_train_bbx_gt.txt",
+        "val": "wider_face_val_bbx_gt.txt",
+        "test": "wider_face_test_filelist.txt",
+    }
+
+    for ds_name in os.listdir(dataset_path):
+        if ds_name == "wider_face_split":
+            continue
+        ds_path = os.path.join(dataset_path, ds_name)
+        if dir_exists(ds_path):
+            ds_name = ds_name.split("_")[1]
+            dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+            if ds_name != "test":
+                anns_path = os.path.join(anns_folder, ds_to_anns[ds_name])
+                im_path_to_bboxes = defaultdict(list)
+                with open(anns_path) as f:
+                    content = f.read().split("\n")
+
+                    for curr_data in content:
+                        if curr_data.find("/") != -1:
+                            curr_im_path = os.path.join(ds_path, "images", curr_data)
+                            continue
+                        if curr_data.find(" ") != -1:
+                            im_path_to_bboxes[curr_im_path].append(curr_data.split(" ")[:4])
+
+                images_pathes = list(im_path_to_bboxes.keys())
+
+            else:
+                images_pathes = glob.glob(ds_path + "/*/*/*.jpg")
+
+            progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+            for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+                img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
+
+                img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                if ds_name != "test":
+                    anns = [create_ann(image_path) for image_path in img_pathes_batch]
+                    api.annotation.upload_anns(img_ids, anns)
+
+                progress.iters_done_report(len(img_names_batch))
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,50 +13,64 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "WIDER FACE"
+PROJECT_NAME_FULL: str = "WIDER FACE: A Face Detection Benchmark"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_NC_ND_2_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.General()]
+CATEGORY: Category = Category.General()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2016
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "http://shuoyang1213.me/WIDERFACE"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 2639222
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/wider-face"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "WIDER Face Training Images": "https://drive.google.com/file/d/15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M/view?usp=sharing",
+    "WIDER Face Validation Images": "https://drive.google.com/file/d/1GUCogbp16PMGa39thoMMeWxp7Rp5oM8Q/view?usp=sharing",
+    "WIDER Face Testing Images": "https://drive.google.com/file/d/1HIfDbVEWKmsYKJZm4lchTBDLW5N7dY5T/view?usp=sharing",
+    "Face annotations": "http://shuoyang1213.me/WIDERFACE/support/bbx_annotation/wider_face_split.zip",
+    "Examples and formats of the submissions": "http://shuoyang1213.me/WIDERFACE/support/example/Submission_example.zip",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
-PAPER: Optional[Union[str, List[str]]] = None
+PAPER: Optional[
+    Union[str, List[str]]
+] = "https://openaccess.thecvf.com/content_cvpr_2016/papers/Yang_WIDER_FACE_A_CVPR_2016_paper.pdf"
 BLOGPOST: Optional[Union[str, List[str]]] = None
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "http://shuoyang1213.me/WIDERFACE/"
+AUTHORS: Optional[List[str]] = ["Shuo Yang", "Ping Luo", "Chen Change Loy", "Xiaoou Tang"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Chinese University of Hong Kong",
+    "Shenzhen Institutes of Advanced Technology, China",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://www.cuhk.edu.hk/",
+    "http://english.siat.cas.cn/",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with value:str to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
